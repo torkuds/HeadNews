fe71195f55edb11bf2363c939b61cd9f24ead9a9
torkuds <iwangtianchao@163.com>
2017-03-25 00:22:50 +0800

    Initial commit

---
+++ app/src/main/java/com/jaydenxiao/headnews/app/AppApplication.java
@@ -0,0 +1,17 @@
+package com.jaydenxiao.headnews.app;
+
+import com.jaydenxiao.headnews.BuildConfig;
+import com.jaydenxiao.common.baseapp.BaseApplication;
+import com.jaydenxiao.common.commonutils.LogUtils;
+
+/**
+ * APPLICATION
+ */
+public class AppApplication extends BaseApplication {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        //初始化logger
+        LogUtils.logInit(BuildConfig.LOG_DEBUG);
+    }
+}
---
+++ app/src/main/java/com/jaydenxiao/headnews/ui/main/model/NewsMainModel.java
@@ -0,0 +1,39 @@
+package com.jaydenxiao.headnews.ui.main.model;
+
+import com.jaydenxiao.headnews.app.AppApplication;
+import com.jaydenxiao.headnews.app.AppConstant;
+import com.jaydenxiao.headnews.bean.NewsChannelTable;
+import com.jaydenxiao.headnews.db.NewsChannelTableManager;
+import com.jaydenxiao.headnews.ui.main.contract.NewsMainContract;
+import com.jaydenxiao.common.baserx.RxSchedulers;
+import com.jaydenxiao.common.commonutils.ACache;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import rx.Observable;
+import rx.Subscriber;
+
+/**
+ * des:
+ * Created by xsf
+ * on 2016.09.17:05
+ */
+public class NewsMainModel implements NewsMainContract.Model {
+    @Override
+    public Observable<List<NewsChannelTable>> lodeMineNewsChannels() {
+
+        return Observable.create(new Observable.OnSubscribe<List<NewsChannelTable>>() {
+            @Override
+            public void call(Subscriber<? super List<NewsChannelTable>> subscriber) {
+                ArrayList<NewsChannelTable> newsChannelTableList = (ArrayList<NewsChannelTable>) ACache.get(AppApplication.getAppContext()).getAsObject(AppConstant.CHANNEL_MINE);
+               if(newsChannelTableList==null){
+                   newsChannelTableList= (ArrayList<NewsChannelTable>) NewsChannelTableManager.loadNewsChannelsStatic();
+                   ACache.get(AppApplication.getAppContext()).put(AppConstant.CHANNEL_MINE,newsChannelTableList);
+               }
+                subscriber.onNext(newsChannelTableList);
+                subscriber.onCompleted();
+            }
+        }).compose(RxSchedulers.<List<NewsChannelTable>>io_main());
+    }
+}
